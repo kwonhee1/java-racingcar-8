40c44ced5427439a87612c03acaad9f40c3ed57d
kwonhee1 <lkh8033@gmail.com>
2025-10-25 15:51:44 +0900

    fix (SimpleInputView): add input message
    fix (SimpleInputView): add input message
    fix (OutputMessage): add input message

--- src/main/java/racingcar/view/OutputMessage.java
+++ src/main/java/racingcar/view/OutputMessage.java
@@ -7,7 +7,12 @@ public enum OutputMessage {
     RESULT_CAR_POSITION_PART("-"),
 
     RESULT_END("최종 우승자 : %s"),
-    RESULT_END_JOIN_REGEX(", ");
+    RESULT_END_JOIN_REGEX(", "),
+
+    INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    INPUT_RACING_COUNT("시도할 횟수는 몇 회인가요?"),
+
+    ERROR("[ERROR] : %s");
 
     private String message;
     OutputMessage(String message) {
--- src/main/java/racingcar/view/SimpleInputView.java
+++ src/main/java/racingcar/view/SimpleInputView.java
@@ -8,6 +8,7 @@ public class SimpleInputView implements InputView {
 
     @Override
     public List<String> inputCarNames() {
+        System.out.println(OutputMessage.INPUT_CAR_NAMES.getMessage());
         String carNameStrs = Console.readLine();
         String[] names = carNameStrs.split(RacingCarConstant.CAR_NAME_REGEX);
         return List.of(names);
@@ -15,6 +16,7 @@ public class SimpleInputView implements InputView {
 
     @Override
     public Integer inputRacingCount() {
+        System.out.println(OutputMessage.INPUT_RACING_COUNT.getMessage());
         return Integer.parseInt(Console.readLine());
     }
 }
