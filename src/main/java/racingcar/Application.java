2b4a33d1f9ca53d28f25b320b7d2bca231fbbdc9
kwonhee1 <lkh8033@gmail.com>
2025-10-25 15:51:07 +0900

    feat (Application): feat Application main method

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.view.SimpleInputView;
+import racingcar.view.SimpleOutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new RacingController(new SimpleInputView(), new SimpleOutputView()).run();
     }
 }
