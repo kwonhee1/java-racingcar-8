652f27c426d960d234a29ade2d1bf7df89239b0a
kwonhee1 <lkh8033@gmail.com>
2025-10-27 11:59:33 +0900

    fix (CarCapture): set public default constructor

--- src/main/java/racingcar/service/dto/CarCapture.java
+++ src/main/java/racingcar/service/dto/CarCapture.java
@@ -7,7 +7,7 @@ public class CarCapture {
     private String carName;
     private int carPosition;
 
-    private CarCapture(String carName, int carPosition) {
+    public CarCapture(String carName, int carPosition) {
         this.carName = carName;
         this.carPosition = carPosition;
     }
